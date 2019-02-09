059ec90446320f032014ee32226b47733cfea265
kenneths <snipe1331@gmail.com>
2019-02-09 14:01:47 -0700

    auth, db, basic route

---
+++ models/user.js
@@ -0,0 +1,20 @@
+// module.exports = function(sequelize, DataTypes) {
+//   var User = sequelize.define("User", {
+//     username: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: { len: [1, 40] }
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: { len: [1, 80] }
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: { len: [1, 20] }
+//     }
+//   });
+//   return User;
+// };
---
+++ routes/memegenerator.js
@@ -0,0 +1,5 @@
+function memeGenerator() {
+  //do something
+}
+
+module.exports = memeGenerator();
